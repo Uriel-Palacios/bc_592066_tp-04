cbb2509741b0b90f082f78062934f3a69ebc0ff2
Uriel <urielalej@gmail.com>
2022-06-27 11:45:48 -0300

    tp4

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let nombre= prompt('ingrese su nombre.')
+console.log(nombre)
+let genero = prompt('ingrese su genero (1 hombre, 2 mujer o cualquier otro numero para no binario.)')
+if(genero ==1){
+    console.log('bienvenido!!')
+}else if(genero == 2){ 
+    console.log('bienvenida!!')
+}else{
+    console.log('bienvenide!!')
+}
